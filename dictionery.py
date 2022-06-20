57beff4b2ee7de079f6c70d752d893df456bbaf7
Eitan Cohen <44434461+eitancohen1@users.noreply.github.com>
2022-06-20 10:06:46 +0300

    Update dictionery.py

--- dictionery.py
+++ dictionery.py
@@ -1,3 +1,6 @@
+import os
+
+
 myfamily={
     1:{"id":"1","fname":"eitan","lname":"cohen","phone":"1111111"},
     2:{"id":"2","fname":"hadas","lname":"cohen","phone":"2222222"},
@@ -7,31 +10,39 @@ myfamily={
     6:{"id":"6","fname":"hagit","lname":"edri","phone":"6666666"},
     7:{"id":"7","fname":"haim","lname":"falak","phone":"7777777"},
     8:{"id":"8","fname":"barak","lname":"cohen","phone":"8888888"},
+    9:{"id":"9","fname":"eitan","lname":"israel","phone":"9999999"},
+    10:{"id":"10","fname":"hadas","lname":"benyamin","phone":"1111110"},
+    11:{"id":"11","fname":"barak","lname":"benyamin","phone":"1111101"},
+    
 }
 
-def func1(kay,value):
-    
-    for y, x in myfamily.items():
-        if myfamily[y][kay]==value:
-            print(myfamily[y]["fname"],myfamily[y]["lname"],myfamily[y]["phone"])
-
+def func1(key,value):
+    try:
+        for y, x in myfamily.items():
+            if myfamily[y][key]==value:
+                print(myfamily[y]["fname"],myfamily[y]["lname"],myfamily[y]["phone"])
+        x=input("press Enter for another search")
+        os.system('cls')
+    except:
+        print("key or value not correct, please try again")
+        
 
 i=0
 
 while i<10:
-    kay= input("Enter a kay for search: ")
-    if kay=="0" or kay=="exit":
+    
+    key= input("Type in which key you would like to find or press 0 to exit: ")
+    if key=="0" or key=="exit" or key=="out":
         break
-    value= input("Enter a " + kay + " for search: ")
+    value= input("Enter a " + key + " for search: ")
 
-    if kay== "last name":
-        kay="lname"
-    if kay== "first name":
-        kay="fname"
+    if key== "last name" or key == "family":
+        key="lname"
+    if key== "first name" or key == "name":
+        key="fname"
     
     
-    func1(kay,value)
-    
+    func1(key,value)
     
     
     
